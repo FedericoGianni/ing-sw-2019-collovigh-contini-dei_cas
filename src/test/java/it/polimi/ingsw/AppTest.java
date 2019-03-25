aa1aa99887d0697ea32d77fbc899a0126fa1b525
contedigital <alessandro3.contini@mail.pomlimi.it>
2019-03-25 11:23:01 +0100

    Last update

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -16,6 +16,6 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-        System.out.println("federico frocio");
+        System.out.println("User e mail aggiornata");
     }
 }
