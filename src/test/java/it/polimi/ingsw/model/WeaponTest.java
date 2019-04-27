a5b176616e968f252225fe707dd28cb3cb4b1ecd
Dav-11 <10522289@polimi.it>
2019-04-27 15:01:56 +0200

    added isPossessedBy() test

---
+++ src/test/java/it/polimi/ingsw/model/WeaponTest.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.model;
+
+import customsexceptions.WeaponNotLoadedException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeaponTest {
+
+    @Test
+    void isPossessedBy() {
+
+        List<String> playerNames = new ArrayList<>();
+        playerNames.add("Player_1");
+        playerNames.add("Player_2");
+        playerNames.add("Player_3");
+
+        List<PlayerColor> playerColors = new ArrayList<>();
+        playerColors.add(PlayerColor.BLUE);
+        playerColors.add(PlayerColor.PURPLE);
+        playerColors.add(PlayerColor.YELLOW);
+
+        //generating model
+        Model currentModel = new Model(playerNames, playerColors, 2);
+
+        Weapon w = new NormalWeapon("Gun", new ArrayList<AmmoCube>(),new ArrayList<MacroEffect>());
+
+        Model.getPlayer(0).addWeapon(w);
+
+        assertEquals(Model.getPlayer(0),w.isPossessedBy());
+
+        assertEquals(Model.getPlayer(0).getAmmoBag(),w.isPossessedBy().getAmmoBag());
+
+    }
+}
