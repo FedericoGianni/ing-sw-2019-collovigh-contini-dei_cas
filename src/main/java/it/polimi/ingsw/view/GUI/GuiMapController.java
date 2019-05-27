c4da21f828be2e243f03245a636c0bcd910581fc
Contedigital <alessandro3.contini@mail.polimi.it>
2019-05-27 13:56:06 +0200

    Add Map that changes on the player num

--- src/main/java/it/polimi/ingsw/view/GUI/GuiMapController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/GuiMapController.java
@@ -1,8 +1,8 @@
 package it.polimi.ingsw.view.GUI;
 
-import it.polimi.ingsw.view.View;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 
@@ -16,23 +16,32 @@ public class GuiMapController {
 
     @FXML
     GridPane mappozza;
+    @FXML
+    BorderPane pane;
 
     @FXML
     public void initialize() {
 
-        switch(gui.getView().getCacheModel().getMapType())
-        {
-            case 1:
-            {mappozza.setStyle("-fx-background-image: url('/images/Map1.png')"); break;}
-            case 2:
-            {mappozza.setStyle("-fx-background-image: url('/images/Map2.png')"); break;}
-            case 3:
-            {mappozza.setStyle("-fx-background-image: url('/images/Map3.png')"); break;}
-        }
-
-
     }
 
+    public void mapCreator()
+    {
+
+        switch(gui.getView().getCacheModel().getMapType()) {
+            case 1: {
+                mappozza.setStyle("-fx-background-image: url('/images/Map1.png')");
+                break;
+            }
+            case 2: {
+                mappozza.setStyle("-fx-background-image: url('/images/Map2.png')");
+                break;
+            }
+            case 3: {
+                mappozza.setStyle("-fx-background-image: url('/images/Map3.png')");
+                break;
+            }
+        }
+    }
 
 
 
