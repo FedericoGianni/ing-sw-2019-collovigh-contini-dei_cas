a748422f8d5e5bf293855c8f97cc9ed26c3ad006
david <10522289@polimi.it>
2019-06-10 15:31:46 +0200

    defined shootAction Class

--- src/main/java/it/polimi/ingsw/view/actions/ShootAction.java
+++ src/main/java/it/polimi/ingsw/view/actions/ShootAction.java
@@ -2,30 +2,58 @@ package it.polimi.ingsw.view.actions;
 
 import it.polimi.ingsw.model.map.Directions;
 
+import java.awt.*;
+import java.util.List;
+
 public class ShootAction extends JsonAction{
 
-    private final int targetId;
+
     private final Directions move;
 
-    public ShootAction(int targetId) {
+    private final List<List<Integer>> targetIds;
+    private final List<Integer> effects;
+    private final List<Point> cells;
+
+    public ShootAction(List<List<Integer>> targetIds, List<Integer> effects, List<Point> cells) {
 
         super(ActionTypes.SHOOT);
-
-        this.targetId = targetId;
 
         this.move = null;
+
+        this.targetIds = targetIds;
+
+        this.effects = effects;
+
+        this.cells = cells;
+
     }
 
-    public ShootAction( int targetId, Directions move) {
+    public ShootAction( Directions move, List<List<Integer>> targetIds, List<Integer> effects, List<Point> cells ) {
 
         super(ActionTypes.SHOOT);
 
-        this.targetId = targetId;
+        this.targetIds = targetIds;
 
         this.move = move;
+
+        this.effects = effects;
+
+        this.cells = cells;
     }
 
-    public int getTargetId() { return targetId; }
+    public Directions getMove() {
+        return move;
+    }
 
-    public Directions getMove() { return move; }
+    public List<List<Integer>> getTargetIds() {
+        return targetIds;
+    }
+
+    public List<Integer> getEffects() {
+        return effects;
+    }
+
+    public List<Point> getCells() {
+        return cells;
+    }
 }
