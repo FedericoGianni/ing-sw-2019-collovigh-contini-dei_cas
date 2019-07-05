d8300bf7b75692fa7fe52998425b29d1864afa7d
Dav-11 <10522289@polimi.it>
2019-07-05 18:18:25 +0200

    completed javadoc until Hellion ( comprehended )

--- src/main/java/it/polimi/ingsw/model/powerup/TagbackGrenade.java
+++ src/main/java/it/polimi/ingsw/model/powerup/TagbackGrenade.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.model.powerup;
 
-import it.polimi.ingsw.customsexceptions.CardNotPossessedException;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.utils.Color;
 import it.polimi.ingsw.utils.PowerUpType;
@@ -28,7 +27,8 @@ public class TagbackGrenade extends PowerUp {
      * ( all the controls will be done in the controller)
      * @see it.polimi.ingsw.controller.PowerUpPhase#useGrenade(GrenadeAction)
      *
-     * @param p is the player at which the grenade will be applied
+     * @param target is the player at which the grenade will be applied
+     * @param sourcePId is the id of the player who used it
      */
     public void applyOn(Player target, int sourcePId) {
 
